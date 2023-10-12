fff53dfddf0d85e0b28610dd83d2d3bee25034b3
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-10-11 18:40:45 -0600

    alarm clocks

---
+++ Source/ZClocks/PlaceWorker_OnSurface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ZClocks
+{
+    class PlaceWorker_OnSurface : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            List<Thing> atLoc = map.thingGrid.ThingsAt(loc).ToList();
+
+            if (atLoc == null)
+            {
+                //return "BMT_PlaceOnDisplay".Translate();
+                return true;
+            }
+
+            foreach (Thing t in atLoc)
+            {
+                if (t.def.building != null)
+                {
+                    if (t.def.surfaceType == SurfaceType.None && t.def.IsEdifice())
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
